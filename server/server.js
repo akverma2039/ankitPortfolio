9b0e00b4e65bde2b401c98cac262db64667e249f
Ankit Kumar Verma <akverma2039@gmail.com>
2025-02-26 17:44:10 +0530

    server.js updated

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'https://your-netlify-site.netlify.app',
+    origin: 'https://ankitkumarverma.netlify.app/',
     credentials: true,
   })
 );
